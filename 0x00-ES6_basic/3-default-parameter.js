88aaa6bcaaf3f53cab64b91e66db22310814dc42
Pule <proceedingmc@gmail.com>
2024-03-04 22:30:48 +0200

    default parameter

---
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -0,0 +1,8 @@
+// Define a function called getSumOfHoods with three parameters: initialNumber, expansion1989, and expansion2019
+export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
+  // If the parameter expansion1989 is not provided, set its value to 89
+  // If the parameter expansion2019 is not provided, set its value to 19
+  // Return the sum of initialNumber, expansion1989, and expansion2019
+  return initialNumber + expansion1989 + expansion2019;
+}
+
