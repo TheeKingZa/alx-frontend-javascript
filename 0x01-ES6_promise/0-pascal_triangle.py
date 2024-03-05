a6d5c617f64314332e071a46753cec5f18182df4
Pule <proceedingmc@gmail.com>
2024-03-05 17:46:34 +0200

    pascal

--- 0x01-ES6_promise/0-pascal_triangle.py
+++ 0x01-ES6_promise/0-pascal_triangle.py
@@ -8,7 +8,8 @@ def pascal_triangle(n):
 
     Returns:
         list: A list of lists representing Pascal's triangle up to the nth row.
-              Each inner list contains the elements of a single row of the triangle.
+              Each inner list contains the elements of a single
+              row of the triangle.
 
     Raises:
         ValueError: If n is less than or equal to 0.
@@ -20,11 +21,13 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]  # Start each row with 1
         for j in range(1, i):
-            # Calculate the value for each element in the row using the values from the previous row
+            # Calculate the value for each element in the row using
+            # the values from the previous row
             row.append(triangle[i-1][j-1] + triangle[i-1][j])
         row.append(1)  # End each row with 1
         triangle.append(row)  # Add the completed row to the triangle
     return triangle
+
 
 def print_triangle(triangle):
     """
@@ -38,7 +41,3 @@ def print_triangle(triangle):
     """
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
-
-if __name__ == "__main__":
-    print_triangle(pascal_triangle(5))  # Print Pascal's triangle up to the 5th row
-
