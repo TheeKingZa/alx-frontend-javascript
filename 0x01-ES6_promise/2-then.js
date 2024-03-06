e652462658aa46c4c828c345c8e58770ef0fb4f9
Pule <proceedingmc@gmail.com>
2024-03-07 00:42:34 +0200

    then

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,18 @@
+// Define a function named handleResponseFromAPI which takes a promise parameter
+export default function handleResponseFromAPI(promise) {
+    // Append three handlers to the promise using .then() method
+    promise
+      .then(() => {
+        // Log a message to the console when the Promise resolves
+        console.log('Got a response from the API');
+        // Return an object with status 200 and body 'success'
+        return { status: 200, body: 'success' };
+      })
+      .catch(() => {
+        // Log a message to the console when the Promise rejects
+        console.log('Got a response from the API');
+        // Return an empty Error object
+        return new Error();
+      });
+  }
+  
