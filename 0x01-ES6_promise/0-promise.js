3e9dd0cb7200fd429665aa690346efd1239f7b1f
Pule <proceedingmc@gmail.com>
2024-03-07 00:27:12 +0200

    promise keep

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    // Perform asynchronous task here, such as fetching data from an API
+    // For demonstration purposes, let's resolve the Promise immediately
+    resolve('Response from API');
+  });
+}
+
