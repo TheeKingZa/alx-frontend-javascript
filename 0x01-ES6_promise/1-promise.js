f799d3aa7737238445524f1a23032e33f6094ba0
Pule <proceedingmc@gmail.com>
2024-03-07 00:40:14 +0200

    1-promise.js

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,15 @@
+// Define a function named getFullResponseFromAPI which takes a boolean parameter 'success'
+export default function getFullResponseFromAPI(success) {
+    // Return a new Promise, which takes two parameters: resolve and reject
+    return new Promise((resolve, reject) => {
+      // Check if the 'success' parameter is true
+      if (success) {
+        // If true, resolve the Promise with an object containing status 200 and the body 'Success'
+        resolve({ status: 200, body: 'Success' });
+      } else {
+        // If false, reject the Promise with an Error object containing the message 'The fake API is not working currently'
+        reject(new Error('The fake API is not working currently'));
+      }
+    });
+  }
+  
