20e127a27bababfdcca8daed008cb1d10f95d503
Pule <proceedingmc@gmail.com>
2024-03-07 00:46:35 +0200

    all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default async function handleProfileSignup() {
+  try {
+    // Concurrently execute both promises and await their resolution
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+
+    // Log the combined result to the console
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    // Log an error message if any of the promises reject
+    console.log('Signup system offline');
+  }
+}
