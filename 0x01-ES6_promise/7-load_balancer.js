03abde6829d704fa1603ee29a53ebcd329950f1c
Pule <proceedingmc@gmail.com>
2024-03-07 00:52:51 +0200

    loadBalancer

---
+++ 0x01-ES6_promise/7-load_balancer.js
@@ -0,0 +1,5 @@
+export default function loadBalancer(chinaDownload, USDownload) {
+    // Return a Promise that resolves with the value of the first resolved Promise
+    return Promise.race([chinaDownload, USDownload]);
+  }
+  
