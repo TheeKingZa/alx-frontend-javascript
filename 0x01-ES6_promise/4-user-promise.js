f457b37bd68fad3a6f91b0c5f7b099fa80771aa6
Pule <proceedingmc@gmail.com>
2024-03-07 00:48:48 +0200

    4-user-promise.js

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,12 @@
+// Define a function signUpUser that takes two parameters: firstName and lastName
+function signUpUser(firstName, lastName) {
+    // Return a Promise object that resolves with an object containing the provided firstName and lastName
+    return Promise.resolve({
+      firstName: firstName,
+      lastName: lastName
+    });
+  }
+  
+  // Export the signUpUser function as the default export of this module
+  export default signUpUser;
+  
